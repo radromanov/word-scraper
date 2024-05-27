915068c59291a83d207f2a2a6e49e8d7861c1b32
Radostin Romanov <rad.romanov@gmail.com>
2024-05-27 16:22:03 +0300

    Can now seed words

--- lib/db/seed.ts
+++ lib/db/seed.ts
@@ -1,7 +1,7 @@
 import { db } from "./index";
 import type { Words } from "../types";
 import { categoriesTable, vocabularyTable } from "./schema";
-import { duration } from "../helpers";
+import { ALPHABET, duration } from "../helpers";
 import { eq } from "drizzle-orm";
 
 async function getFile(): Promise<Words> {
@@ -23,6 +23,25 @@ async function createCategories(data: Words) {
   }
 }
 
+async function createWords(data: Words) {
+  const letters = Object.keys(data);
+
+  for (const letter of letters) {
+    const words = Object.keys(data[letter as (typeof ALPHABET)[number]]);
+    const [category] = await db
+      .select({ id: categoriesTable.id })
+      .from(categoriesTable)
+      .where(eq(categoriesTable.letter, letter));
+
+    for (const word of words) {
+      await db
+        .insert(vocabularyTable)
+        .values({ word, categoryId: category.id })
+        .onConflictDoNothing();
+    }
+  }
+}
+
 async function seed() {
   console.log("🌱 Seeding database...");
 
@@ -31,6 +50,7 @@ async function seed() {
   const data = await getFile();
 
   await createCategories(data);
+  await createWords(data);
 
   const end = performance.now();
   const time = duration(end - start);
